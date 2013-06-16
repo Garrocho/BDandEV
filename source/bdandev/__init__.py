47baf10116d75348b85ef16a095e748d190a00e0
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-16 15:25:17 -0300

    adicionado: arquivo padrão do pacote do projeto bdandev. #1

---
+++ source/bdandev/__init__.py
@@ -0,0 +1,14 @@
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2013 Python Software Open Source
+
+"""
+Este e o pacote do projeto bdandev.
+"""
+
+__version__ = '0.0.1'
+
+
+__all__ = [
+    'detector',
+]
