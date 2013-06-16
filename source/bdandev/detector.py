52ede8750fc2b40866646b66c05462d1b1648d38
Charles Tim Batista Garrocho <charles.garrocho@gmail.com>
2013-06-16 17:07:29 -0300

    corrigido: nome de variável incorreta ao desenha o rectangle na imagem. #1

--- source/bdandev/detector.py
+++ source/bdandev/detector.py
@@ -13,10 +13,10 @@ cascade = cv.Load("../data/todo_corpo.xml")
 
 while True:
     imagem = cv.QueryFrame(camera)
-    corpos_detectados = cv.HaarDetectObjects(imagem, cascade, cv.CreateMemStorage(), 1.2,2, cv.CV_HAAR_DO_CANNY_PRUNING, (0,0) )
+    corpos_detectados = cv.HaarDetectObjects(imagem, cascade, cv.CreateMemStorage(), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (0, 0) )
     
-    for ((x,y,w,h),stub) in corpos_detectados:
-        cv.Rectangle(frame,(int(x),int(y)),(int(x)+w,int(y)+h),(0,255,0),2,0)
+    for ((x,y,w,h), stub) in corpos_detectados:
+        cv.Rectangle(imagem, (int(x), int(y)), (int(x) + w, int(y) + h), (0, 255, 0), 2, 0)
 
     cv.ShowImage("Janela", imagem)
     c = cv.WaitKey(1)
