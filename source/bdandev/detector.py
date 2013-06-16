fca7e56363b1ae8500afbc6108c3fa61230ed336
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-16 15:25:46 -0300

    adicionado: módulo para detecção de corpos. #1

---
+++ source/bdandev/detector.py
@@ -0,0 +1,24 @@
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2013 Python Software Open Source
+
+"""
+Este e o modulo que detecta um corpo.
+"""
+
+import cv2.cv as cv
+
+camera = cv.CaptureFromCAM(0)
+cascade = cv.Load("../data/todo_corpo.xml")
+
+while True:
+    imagem = cv.QueryFrame(camera)
+    corpos_detectados = cv.HaarDetectObjects(imagem, cascade, cv.CreateMemStorage(), 1.2,2, cv.CV_HAAR_DO_CANNY_PRUNING, (0,0) )
+    
+    for ((x,y,w,h),stub) in corpos_detectados:
+        cv.Rectangle(frame,(int(x),int(y)),(int(x)+w,int(y)+h),(0,255,0),2,0)
+
+    cv.ShowImage("Janela", imagem)
+    c = cv.WaitKey(1)
+    if c == 27:
+        break
