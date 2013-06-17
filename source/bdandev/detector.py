ae4fb920d10056cd4d8420b12a9a7c9ac71d3b73
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-17 10:32:32 -0300

    adicionado: constantes para auxiliar o processo de detecção de corpos. #1

--- source/bdandev/detector.py
+++ source/bdandev/detector.py
@@ -9,33 +9,33 @@ Este e o modulo que detecta um corpo.
 import cv2.cv as cv
 from datetime import datetime
 
-tamanho_menor = (20, 20)
-escala_imagem = 2
-escala_haar = 1.2
-minimo_vizinhos = 2
-bandeiras_haar = 0
+TAMANHO_MENOR = (20, 20)
+ESCALA_IMAGEM = 2
+ESCALA_HAAR = 1.2
+MINIMO_VIZINHOS = 2
+BANDEIRAS_HAAR = 0
+
 
 def detectar_corpos(imagem, cascade):
 
     # Criando imagens temporarias.
     gray = cv.CreateImage((imagem.width, imagem.height), 8, 1)
-    imagem_pequena = cv.CreateImage((cv.Round(imagem.width / escala_imagem), cv.Round(imagem.height / escala_imagem)), 8, 1)
+    imagem_pequena = cv.CreateImage((cv.Round(imagem.width / ESCALA_IMAGEM), cv.Round(imagem.height / ESCALA_IMAGEM)), 8, 1)
 
     # Converte a imagem em tons de cinza.
     cv.CvtColor(imagem, gray, cv.CV_BGR2GRAY)
 
     # Redimensiona imagem para um rapido processamento.
     cv.Resize(gray, imagem_pequena, cv.CV_INTER_LINEAR)
-
     cv.EqualizeHist(imagem_pequena, imagem_pequena)
 
-    return cv.HaarDetectObjects(imagem_pequena, cascade, cv.CreateMemStorage(0), escala_haar, minimo_vizinhos, bandeiras_haar, tamanho_menor)
+    return cv.HaarDetectObjects(imagem_pequena, cascade, cv.CreateMemStorage(0), ESCALA_HAAR, MINIMO_VIZINHOS, BANDEIRAS_HAAR, TAMANHO_MENOR)
 
 
 def colorir(imagem, corpos_detectados):
     for ((x, y, w, h), n) in corpos_detectados:
-        pt1 = (int(x * escala_imagem), int(y * escala_imagem))
-        pt2 = (int((x + w) * escala_imagem), int((y + h) * escala_imagem))
+        pt1 = (int(x * ESCALA_IMAGEM), int(y * ESCALA_IMAGEM))
+        pt2 = (int((x + w) * ESCALA_IMAGEM), int((y + h) * ESCALA_IMAGEM))
         cv.Rectangle(imagem, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
     return imagem
 
@@ -44,7 +44,7 @@ def escrever(imagem, texto):
     fonte = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 2, 8)
     cv.PutText(imagem, texto, (25,30), fonte, 0)
     return imagem
-    
+
 
 if __name__ == '__main__':
 
