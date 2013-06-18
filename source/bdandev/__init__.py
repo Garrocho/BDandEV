35863140426bb8de3cc77ce96fa0b721867d20e0
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-18 08:24:57 -0300

    adicionado: módulo estima_values em __all__. #2

--- source/bdandev/__init__.py
+++ source/bdandev/__init__.py
@@ -1,10 +1,11 @@
 """
-Este e o pacote do projeto bdandev.
+Pacote do projeto BDandEV. Contem informacoes do projeto, como por exemplo: versao e modulos.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 __all__ = [
     'body_detect',
+    'estimate_values',
 ]
