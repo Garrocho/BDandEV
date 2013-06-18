7029828165a6f0899c7664b1946dc4331181e2d2
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-18 08:40:09 -0300

    corrigido: nome de variável path nos nomes dos enderecos DATA.

--- source/bdandev/settings.py
+++ source/bdandev/settings.py
@@ -8,9 +8,9 @@ from os import path
 PATH_FONTE = path.abspath(path.dirname(__file__))
 
 # Endereco de dados diversos no projeto.
-PATH_DATA = path.normpath(path.join(path_dados, '..', 'data'))
+PATH_DATA = path.normpath(path.join(PATH_FONTE, '..', 'data'))
 
 # Endereco dos arquivos XML
-CORPO_INTEIRO = path.join(path, 'corpo_inteiro.xml')
-PARTE_SUPERIOR_CORPO = path.join(path, 'parte_superior_corpo.xml')
-PARTE_INFERIOR_CORPO = path.join(path, 'parte_inferior_corpo.xml')
+CORPO_INTEIRO = path.join(PATH_DATA, 'corpo_inteiro.xml')
+PARTE_SUPERIOR_CORPO = path.join(PATH_DATA, 'parte_superior_corpo.xml')
+PARTE_INFERIOR_CORPO = path.join(PATH_DATA, 'parte_inferior_corpo.xml')
